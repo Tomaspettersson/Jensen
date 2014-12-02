50b171a7c12e1f8f9ad2bec8f7b4e32a49081c02
andreasweckman <andreasweckman@gmail.com>
2014-12-02 13:42:11 +0100

    added new help page for logged out mode

---
+++ hjalputloggad.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Jensen Online</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+   
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+
+<body>
+    
+ <nav class="navbar navbar-inverse">
+        <div class="container">
+           <a class="navbar-brand" href="#">Jensen Online</a>
+           <a id="hjalp" class="navbar-brand" href="http://127.0.0.1/git/Jensen/login.php">Tillbaka</a>
+        </div>
+    </nav>
+    
+    
+    <header class="page-header">
+    </header>
+    
+    
+    
+    <main class="jumbotron">
+        <div class="container">
+            <h1>FAQ</h1>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+            <div class="faq-question">
+            "Jag kan inte logga in på min sida?"
+            </div>
+            <div class="faq-answer">
+            <p class="faq">Om du har problem att logga in på grund utav att dina          uppgifter inte stämmer, var vänligt kontakta din systemadministratör</p>
+            </div>
+        </div>
+    </main>
