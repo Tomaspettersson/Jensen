9e8cb5315e3374a3e73307eeece3f51591928e5d
andreasweckman <andreasweckman@gmail.com>
2014-12-08 11:51:19 +0100

    added new content in min klass

--- kurser.php
+++ kurser.php
@@ -18,11 +18,213 @@ include("includes/header.php");
     
 ?>
          
-    <main class="jumbotron">
-   
+ <main class="jumbotron">
+
+
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+<div class="container">
+	<div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Se kursplanen för respektive klass genom att klicka på klassen nedan:</h3>
+        </div>   
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-1" data-toggle="detail-1">
+                    <div class="col-xs-10">
+                        WUK13A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-1">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-2" data-toggle="detail-2">
+                    <div class="col-xs-10">
+                        WUK14A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-2">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+
+
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-3" data-toggle="detail-3">
+                    <div class="col-xs-10">
+                        WUK15A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-3">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+	</div>
+</div>
         
-        
-    <script src="app.js"></script>
+    <script src="js/app.js"></script>
     </main>
 
 <?php
--- minklass.php
+++ minklass.php
@@ -19,67 +19,234 @@ include("includes/header.php");
 ?>
          
     <main class="jumbotron">
-    <div class="table-responsive">
+
+
+<form action="minklass.php" method="post" enctype="multipart/form-data">
+    Välj schema för uppladdning (När schemat är uppladdat kan eleverna själva se schemat):
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Ladda upp schema" name="submit">
+</form><br /><br />
         
-                
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+?>
+
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 <div class="container">
-    <div class="row clearfix">
-    	<div class="col-md-12 table-responsive">
-			<table class="table table-bordered table-hover table-sortable" id="tab_logic">
-				<thead>
-					<tr >
-						<th class="text-center">
-							Namn
-						</th>
-						<th class="text-center">
-							Efternamn
-						</th>
-						<th class="text-center">
-							Personnr
-						</th>
-    					<th class="text-center">
-							Email
-						</th>
-                        <th class="text-center">
-							Behörighet
-						</th>
-        				<th class="text-center" style="border-top: 1px solid #ffffff; border-right: 1px solid #ffffff;">
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-    				<tr id='addr0' data-id="0" class="hidden">
-						<td data-name="name">
-						    <input type="text" name='name0'  placeholder='Förnamn' class="form-control"/>
-						</td>
-						<td data-name="mail">
-						    <input type="text" name='mail0' placeholder='Efternamn' class="form-control"/>
-						</td>
-						<td data-name="desc">
-						    <input type="text" name="Personnr" placeholder="Personnr" class="form-control"></textarea>
-						</td>
-                        <td data-name="mail">
-						    <input type="Email" name='name0'  placeholder='Email' class="form-control"/>
-						</td>
-    					<td data-name="sel">
-						    <select name="sel0">
-        				        <option value"">Select Option</option>
-    					        <option value"1">Behörighet 1</option>
-        				        <option value"2">Behörighet 2</option>
-        				        <option value"3">Behörighet 3</option>
-						    </select>
-						</td>
-                        <td data-name="del">
-                            <button nam"del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
-                        </td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+	<div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Se klasslista genom att klicka på respektive klass nedan</h3>
+        </div>   
+        <ul class="list-group">
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-1" data-toggle="detail-1">
+                    <div class="col-xs-10">
+                        WUK13A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-1">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-2" data-toggle="detail-2">
+                    <div class="col-xs-10">
+                        WUK14A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-2">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+
+
+
+            <li class="list-group-item">
+                <div class="row toggle" id="dropdown-detail-3" data-toggle="detail-3">
+                    <div class="col-xs-10">
+                        WUK15A
+                    </div>
+                    <div class="col-xs-2"><i class="fa fa-chevron-down pull-right"></i></div>
+                </div>
+                <div id="detail-3">
+                    <hr></hr>
+                    <div class="container">
+                        <div class="fluid-row">
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                              <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                            <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                           <div class="col-md-6">
+                                Namn: Tomas Pettersson Personnr: 950101-1010
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
 	</div>
-    	<a id="add_row" class="btn btn-default pull-right">Add Row</a>
 </div>
-        
         
     <script src="js/app.js"></script>
     </main>
