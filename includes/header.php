86279d0704e9c5b08354d6a3919eb72acbda2d2d
Tomaspettersson <tomas.pettersson00@gmail.com>
2014-12-09 12:18:25 +0100

    fixed navbar

--- includes/header.php
+++ includes/header.php
@@ -20,35 +20,73 @@ $_SESSION['time'] = time();
 <!DOCTYPE html>
 <html>
 <head>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
   <meta charset="UTF-8">
   <title><?php echo $pageTitle ?></title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="css/style.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet'  type='text/css'>
 </head>
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     
-    <nav class="navbar navbar-custom">
-        <div class="container">
-           <a class="navbar-brand" href="#">Jensen Online</a>
-
-            <a id="hjalp" class="navbar-brand" href="logout.php">Logga ut</a>
-           <a id="hjalp" class="navbar-brand" href="http://127.0.0.1/git/Jensen/hjalp.php">Hjälp</a>  
- 
-  <div class="dropdown">
-      <a id="hjalp" class="navbar-brand dropdown-toggle" id="dropdownMenu1" aria-expanded="true" href="">
-          <?php echo $_SESSION['fname']; ?><span class="caret"></span>
-      </a><ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
     
-    </ul>
-</div>
+    
+ <nav class="navbar navbar-custom" role="navigation">
+     <div class="container">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
 
-
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $_SESSION['fname']; ?> <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="logout.php">Logga ut</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
         </div>
-    </nav>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+         </div>
+</nav>
 
 
 <?php
