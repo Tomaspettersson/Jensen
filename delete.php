524da73c5650d0e555aa99cb273a7d77b2c090de
Tomaspettersson <tomas.pettersson00@gmail.com>
2015-01-12 12:00:12 +0100

    Added delete and edit function to room booking

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+include('config.php');
+if(isset($_GET['id']))
+{
+$id=$_GET['id'];
+$query1=mysql_query("delete from room_booking where id='$id'");
+if($query1)
+{
+header('location:kalender.php');
+}
+}
+?>
